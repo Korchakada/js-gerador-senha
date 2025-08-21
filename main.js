9d5e518441b4d7e70b8f8a35775b025172da9d0d
Korchakada <163421332+Korchakada@users.noreply.github.com>
2025-08-21 00:10:24 -0300

    checkbox

--- main.js
+++ main.js
@@ -24,11 +24,29 @@ function aumentaTamanho(){
 }
 
 const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
 
 const letrasMaiuscula = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+
 geraSenha();
 
 function geraSenha(){
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaisculas;
+    }
+        if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+        if (checkbox[2].checked){
+        alfabeto = alfabeto + numeros;
+    }
+        if (checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+    }
     let senha = '';
     for (let i = 0; i < tamanhoSenha; i++){
         let numeroAleatorio = Math.random() * letrasMaiuscula.length;
